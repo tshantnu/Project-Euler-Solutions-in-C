f4a2bc555b2e84dcc46af595a6948066a868d966
tshantnu <34413144+tshantnu@users.noreply.github.com>
2019-10-08 13:57:23 +0530

    Add files via upload

---
+++ Problem #1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int i,sum=0;
+    for(i=1;i<1000;i++)
+    {
+        if(i%3==0 || i%5==0)
+            sum+=i;
+    }
+    printf("\n%d ",sum);
+	return 0;
+}
+
